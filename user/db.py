410f9ea1f2b8e4f2c750d1dff78281913f4ffa30
Richard <rcezea007@gmail.com>
2024-02-17 23:43:20 +0100

    update db connection

--- user/db.py
+++ user/db.py
@@ -1,17 +1,15 @@
+import os
+
 import mongoengine
+
+db_url = os.getenv("MONGODB_STRING")
 
 
 def start_db():
-    """ Start the database with name 'forsheets' """
-    mongoengine.register_connection(db='forsheets', alias='core')
+    """ Start the database with name 'forsheet' """
+    mongoengine.connect(db='forsheet', alias='core', host=db_url,)
 
 
 def kill_db():
     """ Kill the database with alias 'core' """
     mongoengine.disconnect('core')
-
-
-
-
-
-
